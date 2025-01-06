b64453d6dfbb152c8ac32adad44caa52766c8d47
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-01-06 20:24:03 +0900

    FirstCommit

---
+++ src/main/java/com/example/demo/HosSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosSearchApplication.class, args);
+	}
+
+}
